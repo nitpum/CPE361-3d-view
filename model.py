f478e54d665aafbc9b7bbf2c1ad9bae1fed3aa5a
nitpum <nitpum@outlook.com>
2020-04-29 13:42:28 +0700

    Add model module

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
-def import_model(file_name, edges, vertices):
+def read_model(file_name, edges, vertices):
     f = open(file_name)
     for line in f.readlines():
         l = line.rstrip('\n').split(' ')
-        name = l[0]
-        if name == 'v':
+        flag = l[0]
+        if flag == 'v':
             vertices.append((float(l[1]), float(l[2]), float(l[3])))
-        elif name == 'f':
+        elif flag == 'f':
             edges_list = l[1:]
             for i in range(len(edges_list)):
                 if i == len(edges_list) - 1:
