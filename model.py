7111e658a64df04e8667cdb4b55cd0f3c7978c40
nitpum <nitpum@outlook.com>
2020-04-29 19:58:31 +0700

    Update model module

--- model.py
+++ model.py
@@ -13,3 +13,23 @@ def read_model(file_name, edges, vertices):
                 else:
                     edges.append((int(edges_list[i]), int(edges_list[i + 1])))
     f.close()
+
+
+def make_model_in_first_quadrant(edges, vertices):
+    min_y = 0
+    min_x = 0
+    min_z = 0
+    for vertice in vertices:
+        x = vertice[0]
+        y = vertice[1]
+        z = vertice[2]
+        if x <= min_x:
+            min_x = x
+        if y <= min_y:
+            min_y = y
+        if z <= min_z:
+            min_z = z
+    for i in range(len(vertices)):
+        vertice = vertices[i]
+        vertice = (vertice[0] - min_x, vertice[1] - min_y, vertice[2] - min_z)
+        vertices[i] = vertice
